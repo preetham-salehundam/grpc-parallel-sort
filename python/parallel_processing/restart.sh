89c1586a49b3ec7532583fb9787ce07c29606e71
preetham <preetham.salehundam@gmail.com>
2019-07-21 17:47:57 -0400

    files renamed and pyc ignored

--- python/parallel_processing/restart.sh
+++ python/parallel_processing/restart.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 kill $(ps -ef | grep parallel_sorting_ | awk '{print $2}')
 
-python parallel_sorting_server.py 50051 &
+python parallel_sorting_worker.py 50051 &
 
-python parallel_sorting_server.py 50052 &
+python parallel_sorting_worker.py 50052 &
 
-python parallel_sorting_server.py 50053 &
+python parallel_sorting_worker.py 50053 &
 
-python parallel_sorting_server.py 50054 &
+python parallel_sorting_worker.py 50054 &
 
-python parallel_sorting_server.py 50055 &
+python parallel_sorting_worker.py 50055 &
 
-python parallel_sorting_server.py 50056 &
+python parallel_sorting_worker.py 50056 &
 
-python parallel_sorting_server.py 50057 &
+python parallel_sorting_worker.py 50057 &
 
-python parallel_sorting_server.py 50058 &
+python parallel_sorting_worker.py 50058 &
 
-python parallel_sorting_server.py 50059 &
+python parallel_sorting_worker.py 50059 &
 
-python parallel_sorting_server.py 50060 &
+python parallel_sorting_worker.py 50060 &
 
 #python -m grpc_tools.protoc --proto_path ../../_protos --python_out=. --grpc_python_out=. ../../_protos/parallel_sorting.proto
